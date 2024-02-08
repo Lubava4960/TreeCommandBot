05eb4f3784631c18d2bf3474055100c80a8a69f9
Lubava <Lwm28@rambler.ru>
2024-02-08 16:14:39 +0300

    first commit

--- src/main/java/TelegramBotApplication.java
+++ src/main/java/TelegramBotApplication.java
@@ -1,15 +1,9 @@
-package org.example;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringApplication
+@SpringBootApplication
 public class TelegramBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(TelegramBotApplication.class, args);
     }}
-
 
