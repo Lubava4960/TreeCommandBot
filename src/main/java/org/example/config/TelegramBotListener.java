8bd151ec257974f40b0dd7b568e7c6821b18d874
Lubava <Lwm28@rambler.ru>
2024-02-08 21:56:47 +0300

    add TelegramBot

---
+++ src/main/java/org/example/config/TelegramBotListener.java
@@ -0,0 +1,81 @@
+package org.example.config;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@EqualsAndHashCode(callSuper = true)
+
+@Data
+@Getter
+@Setter
+@Service
+
+public  class TelegramBotListener extends TelegramLongPollingBot {
+
+    TelegramBotConfiguration config;
+
+    public TelegramBotListener (TelegramBotConfiguration config) {
+
+        this.config = config;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String messageText=update.getMessage().getText();
+            long chatId=update.getMessage().getChatId();
+            switch (messageText){
+                case "/start":
+                    try {
+                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, command was not recognized! ");
+
+
+            }
+        }
+    }
+
+    public void startCommandReceived(long chatId, String name) throws TelegramApiException {
+        String answer = ("Hi," + name+ " nice to meet you! ") ;
+        sendMessage(chatId,answer);
+
+
+    }
+
+    public  void sendMessage(long chatId, String textToSend){
+        SendMessage message=new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        message.setReplyMarkup(message.getReplyMarkup());
+        try {
+            execute(message);
+        }catch (TelegramApiException e){
+
+        }
+
+    }
+
+    @Override
+    public String getBotToken(){
+
+        return config.getToken();
+    }
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+}
+
